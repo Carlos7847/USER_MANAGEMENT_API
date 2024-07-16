371625e024bff34bebd56337bf9e5f279495d65b
Carlos7847 <carlitosmp1597g@gmail.com>
2024-07-15 23:09:34 -0500

    se agrega bloqueo por cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,13 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  const clientUrl = process.env.CLIENT_URL || 'http://localhost:8080/';
+
+  app.enableCors({
+    origin: [clientUrl],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
   app.setGlobalPrefix('/api/v1');
 
   app.useGlobalPipes(
