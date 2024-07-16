a306e9cfc3a50ee5f0f6c189a1f58f7aa994ab3c
Carlos7847 <carlitosmp1597g@gmail.com>
2024-07-15 23:16:28 -0500

    debug

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const clientUrl = process.env.CLIENT_URL || 'http://localhost:8080/';
+  console.log(clientUrl, 'clientUrlga');
 
   app.enableCors({
     origin: [clientUrl],
